2a74f9311b4b756c53b4e2b66daba35b2ec50363
dj_cook <dj_cook@yahoo.com>
2019-09-05 09:49:08 -0500

    Initial

---
+++ src/main/java/com/vaadin/starter/skeleton/MvnTest/App.java
@@ -0,0 +1,14 @@
+package com.vaadin.starter.skeleton.MvnTest;
+import org.json.simple.parser.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	JSONParser parser = new JSONParser();
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
