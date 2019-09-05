5599ddd9d3308c0ec84228f609f6a291d0b6cedc
dj_cook <dj_cook@yahoo.com>
2019-09-05 09:59:26 -0500

    message update

--- src/main/java/com/vaadin/starter/skeleton/MvnTest/App.java
+++ src/main/java/com/vaadin/starter/skeleton/MvnTest/App.java
@@ -9,6 +9,6 @@ public class App
 	JSONParser parser = new JSONParser();
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! from me" );
     }
 }
